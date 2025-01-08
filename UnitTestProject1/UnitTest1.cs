215124b8f752b7710ec49749e789a43b98a1f76f
aS1lenT <tothmartin06@gmail.com>
2025-01-08 11:29:37 +0100

    nsjcnajwk

--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -10,37 +10,37 @@ namespace AtvaltOOP.Tests
         [Test]
         public void TestAtvaltToTizes()
         {
-            var atvalt = new Atvalt("1101");
-            var eredmeny = atvalt.AtvaltToTizes();
+            double atvalt = new Atvalt("1101");
+            double eredmeny = atvalt.AtvaltToTizes();
             Assert.AreEqual("13", eredmeny);
         }
 
         [Test]
         public void TestAtvaltToBinaris()
         {
-            var atvalt = new Atvalt("13");
-            var eredmeny = atvalt.AtvaltToBinaris();
+            double atvalt = new Atvalt("13");
+            double eredmeny = atvalt.AtvaltToBinaris();
             Assert.AreEqual("1101", eredmeny);
         }
 
         [Test]
         public void TestInvalidBinarisInput()
         {
-            var ex = Assert.Throws<ArgumentException>(() => new Atvalt("1234"));
+            double ex = Assert.Throws<ArgumentException>(() => new Atvalt("1234"));
             Assert.AreEqual("Hibás bináris szám.", ex.Message);
         }
 
         [Test]
         public void TestEmptyInput()
         {
-            var ex = Assert.Throws<ArgumentException>(() => new Atvalt(""));
+            double ex = Assert.Throws<ArgumentException>(() => new Atvalt(""));
             Assert.AreEqual("A szám nem lehet üres.", ex.Message);
         }
 
         [Test]
         public void TestInvalidDecimalInput()
         {
-            var ex = Assert.Throws<ArgumentException>(() => new Atvalt("abc"));
+            double ex = Assert.Throws<ArgumentException>(() => new Atvalt("abc"));
             Assert.AreEqual("A tizes szám átváltása nem sikerült.", ex.Message);
         }
     }
